f04922684ba66454bcff74774047b47bd39b5d3b
Vlad Petrov <ejiek@mail.ru>
2020-12-08 15:34:33 +0300

    use proper shell

--- dhcp-server.sh
+++ dhcp-server.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 sudo apt update
 sudo apt install --assume-yes isc-dhcp-server
--- dhcpd-conf.sh
+++ dhcpd-conf.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 sudo mv /tmp/dhcpd.conf /etc/dhcp/dhcpd.conf
 sudo systemctl restart isc-dhcp-server
